7c92defa7d7f062ce8e8e4efd4290e1e6b4cc0c7
Ubuntu <ubuntu@ip-10-244-45-191.ec2.internal>
2013-07-13 20:15:38 +0000

    setting up HW2 and test verification for push

---
+++ initVM.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
